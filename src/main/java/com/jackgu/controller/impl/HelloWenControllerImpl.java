e3beacdd196c6166d2e47df28e4d0178fd1754c4
JackGu <jackgu883@163.com>
2023-04-22 14:11:33 +0800

    master分支提交首次提交！

---
+++ src/main/java/com/jackgu/controller/impl/HelloWenControllerImpl.java
@@ -0,0 +1,19 @@
+package com.jackgu.controller.impl;
+
+import com.jackgu.controller.HelloWebController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HelloWenControllerImpl implements HelloWebController {
+
+    @Override
+    public String sayHello() {
+        String str = "Hello Web :)";
+        //后台日志输出字符串！
+        log.info("======【" + str + "】！");
+        //页面显示返回字符串！
+        return str;
+    }
+}
